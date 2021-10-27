0a94e4ab949bc7e78b9673f48032d99189ddedc0
ShawnChen <chenxyzj@sina.com>
2021-10-27 18:06:09 +0800

    首次提交：1个可以查看float型存储内容的程序。

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(){
+    int a=5;
+    float i = 100.25;
+    printf("a=%d, i=%f\n",a);
+    printf("Hello C from VS Code.\n");
+    a = (unsigned int)i;
+    float *p = &i;
+    unsigned char *q = p;
+    //预期输出:p=6422288(地址), *p=100.250000, i=00,80,c8,42
+    printf("p=%d, *p=%f, i=%02x,%02x,%02x,%02x\n",p,*p,(unsigned char)*q,(unsigned char)*(q+1),(unsigned char)*(q+2),(unsigned char)*(q+3));
+}
